4d76bbaf810e23e60355bf33555f068c6ab5d540
anasikram <anasikram501@gmail.com>
2021-04-23 22:18:41 +0500

    Updates

--- static/js/main.030b36c0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcrownsotre=this.webpackJsonpcrownsotre||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),o=n.n(s),a=(n(8),n(9),n(0));var i=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("header",{className:"App-header"})})};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.030b36c0.chunk.js.map
